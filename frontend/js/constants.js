10f407f406b5622faa675cebb1c58b7a3937c4c0
Spwede <alonso1992@hotmail.co.uk>
2022-09-25 20:55:54 +0100

    h

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x1D16a2B70d0A6d835349777466b846D58CC13f89"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welc ome to the allllQuirky ALiens NFT Project!!";
+const welcome_h1 = "Welcome to the Quirky ALiens mint site!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'Please see twitter page for all info';
+const welcome_p = 'All project info found on twitter!!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
